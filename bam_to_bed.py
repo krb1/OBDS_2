dcb04d7471628ff8c46b4c9cf036792c4cee5634
Katherine Bull <bullk@Ks-MacBook.local>
2019-01-25 15:27:42 +0000

    last version for today, this works

--- bam_to_bed.py
+++ bam_to_bed.py
@@ -10,8 +10,40 @@ import pysam
 import sys
 import argparse
 
-###Hello there 
-a = ['a','b','c']
-print a
+parser = argparse.ArgumentParser(description= "convert bam to bed")
+parser.add_argument('-bamfile', help='bam file input')
+parser.add_argument('-bedfile', help='bed file output', default = 'default.bed')
+args=parser.parse_args()
 
+if args.bedfile == 'default.bed':     
+    args.bedfile=(str(args.bamfile).rsplit(".",1)[0]+".bed")   
+    
+if str(args.bamfile).rsplit(".",1)[1] != "bam":
+    raise Exception('Are you sure this is a bam file?')       
+else:
+    pass
+
+output = open(args.bedfile, 'w')
+count_unmapped = 0
+samfile = pysam.AlignmentFile(args.bamfile, "rb")
+for read in samfile.fetch():
+    if read.is_unmapped == True:
+        count_unmapped += 1
+        continue  
+    #this will be fetch eventually
+    else:
+        chrom = read.reference_name
+        start = read.reference_start
+        stop = read.reference_end
+        name = read.query_name
+        if read.is_reverse == True:
+            strand = "-"
+        else:
+            strand = "+"   
+    
+        bed_line = '{}\t{}\t{}\t{}\t.\t{}\n' .format(chrom, start, stop, name, strand) 
+        output.write(bed_line)
+print(count_unmapped)   
+samfile.close()
+output.close()
 #I made this comment later
